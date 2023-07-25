567cb0b52259032a5ce8c0b8d96615b5630f9e41
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-07-25 12:47:04 -0500

    adding sticky nav-bar

--- js/script.js
+++ js/script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+// Get the navbar element
+const navbar = document.getElementById("desktop-nav");
+
+// Get the offset position of the navbar
+const navbarOffset = navbar.offsetTop;
+
+// Function to add the "sticky" class to the navbar when scrolling
+function stickyNavbar() {
+    if (window.pageYOffset >= navbarOffset) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Call the stickyNavbar function on page load and on scroll
+window.addEventListener("load", stickyNavbar);
+window.addEventListener("scroll", stickyNavbar);
